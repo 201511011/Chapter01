8ccec7e3abc567766878491aa7b4313f2940d3a4
사라 <@172.16.27.110>
2017-01-05 15:03:43 +0900

    소수 판별법

--- src/PrimeDetect.java
+++ src/PrimeDetect.java
@@ -2,34 +2,35 @@ import java.util.*;	// Scanner 클래스를 사용하기 위함
 			
 public class PrimeDetect {
 
-   public static void main(String[] args) {
-
-		Scanner stdin = new Scanner(System.in);
-		System.out.print("소수인지 판단할 숫자: ");
-		int num = stdin.nextInt();
-		int div =2; 
-//	
-//		while(div<num){
-//			while()	
-//				if(num%div==0){
-//					System.out.println(num+"은 소수");
-//				}
-//				else System.out.println(num+"은 소수가 아니다.");
-//				
-//		}
-//		
-		while(num>div){
-			while(num%div!=0){
-				if(num%div==0){
-					System.out.println(num+"은 소수가 아니다");
-					break;
-				}
-				else{
-					div++;
-				}
+	public static void main(String[] args) {
+		Scanner scan= new Scanner(System.in);
+		
+		System.out.print("값을 입력:");
+		int n=scan.nextInt();
+		int div=2;
+		
+		while(n>div){
+			if(n%div==0){
+				System.out.println("소수가 아닙니다");
+				break;
 			}
-			System.out.println(num+"은 소수입니다.");
+			
+			div++;
+			
+			if(n==div){
+				System.out.println("소수 입니다.");
+			}
 		}
-    }	
+		
+		if(n==1||n==2){
+			System.out.println("소수입니다");
+			
+		}
+		
+		
+		
+		
+		
+	}
 }
 
